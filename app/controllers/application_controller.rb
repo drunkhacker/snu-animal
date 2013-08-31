50c237ce0e5398fe2933c5f55359d0dd0f3d5d6c
Taekmin Kim <tantara.tm@gmail.com>
2013-08-31 12:14:15 +0900

    dropdown login, logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def back
+    request.env["HTTP_REFERER"]||"/"
+  end
+
+  def authorized?
+    redirect_to back unless user_signed_in?
+  end
 end
