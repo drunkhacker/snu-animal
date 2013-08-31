d4825871906e4570f825f6e9ce52acdbc775102d
Taekmin Kim <tantara.tm@gmail.com>
2013-08-31 11:54:24 +0900

    HTML5 IE support, twitter bootstrap js dependency error fixed, favicon

--- app/assets/javascripts/application-ie.js
+++ app/assets/javascripts/application-ie.js
@@ -10,19 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require twitter/bootstrap/affix
-//= require twitter/bootstrap/alert
-//= require twitter/bootstrap/button
-//= require twitter/bootstrap/carousel
-//= require twitter/bootstrap/collapse
-//= require twitter/bootstrap/dropdown
-//= require twitter/bootstrap/modal
-//= require twitter/bootstrap/tooltip
-//= require twitter/bootstrap/popover
-//= require twitter/bootstrap/scrollspy
-//= require twitter/bootstrap/tab
-//= require twitter/bootstrap/transition
-//= require_self
+//= require ie/html5shiv
+//= require ie/respond.min
