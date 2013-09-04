09ab25dd6d28e63613500448389e17ee5baaf5d3
Taekmin Kim <tantara.tm@gmail.com>
2013-09-05 00:11:13 +0900

    authorized? flash error 제거

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def authorized?
     unless user_signed_in?
-      flash[:error] = "Not signed in"
+      #flash[:error] = "Not signed in" # deprecated because redirect_to login_path
       redirect_to login_path
     end
   end
