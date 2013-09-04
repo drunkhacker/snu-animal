ad6a08a1b9333ebfbce66b905aa223aba7bf39e0
Taekmin Kim <tantara.tm@gmail.com>
2013-09-04 15:46:14 +0900

    pagination

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,8 @@
 Kaminari.configure do |config|
-  config.default_per_page = 5
+  config.default_per_page = 20
   config.max_per_page = nil
-  config.window = 4
-  config.outer_window = 0
+  config.window = 10
+  config.outer_window = 2
   config.left = 0
   config.right = 0
   config.page_method_name = :page
