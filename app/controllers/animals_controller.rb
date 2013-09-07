e197bd0e96c5247f79d07031fe7dfcc5b5e873e6
Taekmin Kim <tantara.tm@gmail.com>
2013-09-07 22:46:43 +0900

    popover 메세지 변경

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,11 +1,11 @@
 class AnimalsController < ApplicationController
   before_filter :authorized?
   def collection
-    @collection = AnimalCollection.where("user_id = ?", current_user.id).map {|ac| ac.animal}
+    @collection = AnimalCollection.where("user_id = ?", current_user.id)#.map {|ac| ac.animal}
     @taken = []
 
     #manupulate positions for 50 columns
-    @collection.each do |animal|
+    @collection.each do |elem|
       position = rand(30)
       while @taken[position] do
         position = rand(30)
